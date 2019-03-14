7dfd0292e002c0005e16a0ef0b71f0f138003177
BramGoos <bram.goos@hva.nl>
2019-03-14 12:33:25 +0100

    Panel component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,67 +1,59 @@
 <template>
-  <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <v-toolbar dark color="dark">
-            <v-toolbar-title>Login</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-tooltip bottom>
-              <span>Source</span>
-            </v-tooltip>
-          </v-toolbar>
-          <v-card-text>
+<div>
+    <panel title="Login">
+        <v-card-text>
             <v-form>
-              <input type="text" name="username" v-model="user.username" placeholder="Username">
-              <input type="password" name="password" v-model="user.password" placeholder="Password">
+                <v-text-field class="mb-2" type="text" name="username" v-model="user.username" placeholder="Username"></v-text-field>
+                <v-text-field type="password" name="password" v-model="user.password" placeholder="Password"></v-text-field>
             </v-form>
-          </v-card-text>
-          <v-card-actions>
+        </v-card-text>
+        <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn color="primary" @click="login()">Login</v-btn>
-            <h3>{{ response }}</h3>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        </v-card-actions>
+    </panel>
+</div>
 </template>
 
 <script>
 import axios from "axios";
+import Panel from '@/components/Panel.vue'
 
 export default {
-  data() {
-    return {
-      response: [],
-      errors: [],
+    data() {
+        return {
+            response: [],
+            errors: [],
 
-      user: {
-        username: "",
-        password: ""
-      }
-    };
-  },
+            user: {
+                username: "",
+                password: ""
+            }
+        };
+    },
 
-  methods: {
-    login() {
-      var params = new URLSearchParams();
-      params.append("username", this.user.username);
-      params.append("password", this.user.password);
-      console.log(this.user.username);
+    methods: {
+        login() {
+            var params = new URLSearchParams();
+            params.append("username", this.user.username);
+            params.append("password", this.user.password);
+            console.log(this.user.username);
 
-      axios
-        .post(`https://mijn-inzet.herokuapp.com/api/login`, params)
-        .then(response => {
-          // JSON responses are automatically parsed.
-          this.response = response.data;
-          console.log(response);
-        })
-        .catch(e => {
-          this.errors.push(e);
-        });
+            axios
+                .post(`https://mijn-inzet.herokuapp.com/api/login`, params)
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    this.response = response.data;
+                    console.log(response);
+                })
+                .catch(e => {
+                    this.errors.push(e);
+                });
+        }
+    },
+    components: {
+        Panel
     }
-  }
 };
 </script>
 
---
+++ src/components/Panel.vue
@@ -0,0 +1,39 @@
+<template>
+<v-container fluid fill-height>
+    <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+            <v-card class="elevation-6">
+                <v-toolbar 
+                    dark color="dark" 
+                    height="50">
+                    <v-toolbar-title>{{title}}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <slot name="action"></slot>
+                </v-toolbar>
+
+                 <v-card-text>
+            <slot>No slot content defined</slot>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+
+    props: ['title']
+};
+</script>
+
+<style>
+</style>
