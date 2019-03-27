dbcf6989cf9f0aa6b169af0892fb6869b9e7e494
Bram <bram_goos@hotmail.com>
2019-03-27 12:27:36 +0100

    Tiles

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    <h1 mt-5>Home page</h1>
+    <v-container>
+      <v-layout row align-center>
+        <v-flex>
+          <tile-button title="Login"></tile-button>
+          <tile-button title="Login"></tile-button>
+          <tile-button title="Reports"></tile-button>
+          <tile-button title="Rooster"></tile-button>
+          <tile-button title="Instelingen"></tile-button>
+          <tile-button title="Register"></tile-button>
+          <tile-button title="Login"></tile-button>
+          <tile-button title="Login"></tile-button>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+import TileButton from "@/components/TileButton.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    TileButton
+  }
+};
 </script>
 
 <style scoped>
+.tile-buttons {
+  width: 20%;
+  height: 220px;
+}
 </style>
