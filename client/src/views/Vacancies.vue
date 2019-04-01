4d65046c915423b7fac9ec1c230f2ce2231ce55d
Kah Kit <KahKitZheng@gmail.com>
2019-04-01 09:49:58 +0200

    Feature - Add new HeaderTitle component

--- client/src/views/Vacancies.vue
+++ client/src/views/Vacancies.vue
@@ -1,15 +1,24 @@
 <template>
-  <vacancies-dashboard title="Vacatures"></vacancies-dashboard>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <HeaderTitle title="Vacatures" subTitle="* uur beschkbaar"></HeaderTitle>
+        <vacancies-dashboard title="Vacatures"></vacancies-dashboard>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import HeaderTitle from "@/components/HeaderTitle.vue";
 import VacanciesDashboard from "@/components/VacanciesDashboard.vue";
 export default {
   data() {
     return {};
   },
-  components:{
-      VacanciesDashboard
+  components: {
+    HeaderTitle,
+    VacanciesDashboard
   }
 };
 </script>
