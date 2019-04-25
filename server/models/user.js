172e234ce570a38e6af82ae4f036da15f8af53fb
Kah Kit <KahKitZheng@gmail.com>
2019-04-25 14:27:06 +0200

    Auto stash before merge of "feature/available-hours" and "origin/Vacancies-Dashboard"

--- server/models/user.js
+++ server/models/user.js
@@ -1,36 +1,23 @@
-const Joi = require('joi');
-
-module.exports = (sequelize, type) => {
-    return sequelize.define('user', {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type: Sequelize.STRING
-        },
-        password: {
-            type: type.STRING,
-            allowNull: false
-        },
-        roleId:{
-            type: type.INTEGER,
-            allowNull: false
-        }
-    })
-}
-
-function validateUser(user){
-    const schema = {
-        name: Joi.string().min(5).max(50).required(),
-        password: Joi.string().min(5).max(255).required()
-    };
-
-    return Joi.validate(user, schema);
-}
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("user", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    }
+  });
 
   User.associate = models => {
+    User.hasMany(models.UserVacancy, {
+      foreignKey: "user"
+    });
+
     User.hasMany(models.Timeslot, {
       foreignKey: "user_id"
     });
