0bd4290e001c8fa41015a65440b6553466c34008
Bram <bram_goos@hotmail.com>
2019-05-12 18:06:13 +0200

    port

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ dbDebugger("Connected to the database...");
 
 const http = require("http");
 const hostname = "0.0.0.0";
-const port = 80;
+const port = 3306;
 console.log("hostname: " + hostname);
 console.log("port: " + port);
 https.createServer(app).listen(port, hostname, () => {
