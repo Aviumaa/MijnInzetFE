6527977351c748647168adc4bfa43c9144925b8e
Tim <xangxiao@gmail.com>
2019-05-29 13:16:45 +0200

    Feature - tabs using components

--- client/src/components/Tabs.vue
+++ client/src/components/Tabs.vue
@@ -1,31 +1,38 @@
 <template>
   <v-app>
     <v-tabs fixed-tabs>
-      <v-tab to="/tab1">Tet</v-tab>
-      <v-tab to="/tab1">Tets</v-tab>
-      <!-- <v-tabs-items v-model="tabs">
-      <v-tab-item v-for="n in 2" :key="n">
-        <v-card>
-          <router-link to="/tab1"></router-link>
-        </v-card>
-      </v-tab-item>
-      </v-tabs-items>-->
-    </v-tabs>
-    <router-view></router-view>
+      <v-tab
+      v-for="n  in 2"
+      :key="n"
+      ripple  
+      >{{ tabheaders[n-1] }}</v-tab>
+    <v-tab-item
+    v-for="n in 2"
+    :key="n"
+    >
+    <div v-if="n == 1">
+        <Tab1></Tab1>
+    </div>
+    <div v-if="n == 2">
+      <h1>hoi</h1>
+    </div>
+    </v-tab-item>
+  </v-tabs>
   </v-app>
 </template>
 
 <script>
-import Tab1 from "@/components/Tab1.vue";
+import Tab1 from '@/components/Tab1.vue';
+
 export default {
+  components: {
+    Tab1
+  },
   data() {
     return {
       tabs: null,
-      headers: ["Onderwijstaken", "Niet onderwijstaken"]
+      tabheaders: ["Onderwijstaken", "Niet onderwijstaken"]
     };
-  },
-  component: {
-    Tab1
   }
 };
 </script>
