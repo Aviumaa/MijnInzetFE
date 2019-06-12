9ddc22986e921444efde273f2a9e660a7cff57de
Nick Brugemann <nick.brugemann@hva.nl>
2019-06-12 10:59:29 +0200

    wip select file in editedutask

--- client/src/views/EditEducationTask.vue
+++ client/src/views/EditEducationTask.vue
@@ -8,12 +8,30 @@
         <td class="text-xs-left">{{ props.item.type }}</td>
       </template>
     </v-data-table>
+    <!-- SelectFile -->
+    <div style="border: 1px solid black">
+        <div class="btn">
+          <span>Kies een bestand...</span><br>
+          <input id="fileSelector" name="myFile" type="file" multiple="multiple"> 
+        </div>
+        <div>
+          <input type="text">
+        </div>
+        <div>
+            <p id="fileContents"></p>
+        </div>
+    </div>
+    <!-- /SelectFile -->
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Tabs from "@/components/Tabs.vue";
+import SelectFile from "@/components/SelectFile.vue";
+const Papa = require('papaparse');
+
+
 export default {
   data() {
     return {
@@ -33,6 +51,7 @@ export default {
   },
   mounted() {
     this.educationalProgram = this.$route.params.educationalProgram;
+    console.log(this.educationalProgram.id);
     axios
       .get(
         `http://localhost:3000/api/educationalProgramCourse/${
@@ -45,6 +64,34 @@ export default {
       .catch(error => {
         console.log(error);
       });
+
+    let xxid = this.educationalProgram.id;
+    document.getElementById("fileSelector").onchange = function() {
+        let file = document.getElementById("fileSelector").files[0];
+        if (file) {
+            var data = Papa.parse(file, {
+                complete: function(results){
+                    results.data.forEach((row) => {
+                        console.log(row);
+                        if (row[0] != ""){
+                            axios.put("http://localhost:3000/api/course",{
+                                educationalProgramId: xxid,
+                                title: row[0],
+                                ects: row[1],
+                                period: row[2],
+                                type: row[3]
+                            })
+                            .then(response => {
+                                if (response.status === 201) {
+                                    console.log(response);
+                                }
+                            });
+                        }
+                    })
+                }
+            });
+        }
+    }
   }
 };
 </script>
